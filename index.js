cca0a3c530e3676daec5b8f703082626b6248fa0
Zamzam Abdiwahid <zamzamabdiwahid123@gmail.com>
2024-04-04 06:12:13 +0300

    Understanding how functions determine scope in JavaScrip

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function
+ upperCaseCustomerName() {
+    customerName =
+  customerName.toUpperCase();
+ }
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+  console.log(bestCustomer);
+}
+
+const leastFavoriteCustomer = 'zamzam';
+function 
+changeLeastFavoriteCustomer() {
+  {leastFavoriteCustomer = 'zamu';
+    }
+}
+describe('changeLeastFavoriteCustomer()', function() {
+  it('unsuccessfully tries to reassign the least favorite customer', function() {
+    expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+  });
+});
